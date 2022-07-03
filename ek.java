e46a2ebde41b1ef788282e2d4d9b3d7d0fe50d91
DUSARI96 <106464093+DUSARI96@users.noreply.github.com>
2022-07-03 07:34:48 +0530

    task001-030722734
    create java file

---
+++ ek.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class one
+{
+  public static void main(string[] args)
+  {
+    system.out.println("welcome to java application-1");
+    system.out.println("welcome to java application-1");
+    system.out.println("welcome to java application-1");
+    system.out.println("welcome to java application-1");
+  }
+  
+}
